86f63618201808d7b947115f7d0f484f8e5f8e40
casy20 <cas.buckens@outlook.com>
2025-03-12 14:27:05 +0100

    home page gemaakt.

--- index.php
+++ index.php
@@ -1,18 +1,24 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="nl">
-
 <head>
-    <title></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php require_once 'head.php'; ?>
+    <title>Home</title>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
 </head>
-
 <body>
-    
+
+    <div class="navbar">
+        <a href="index.php">HOME</a>
+        <a href="inlog-page.php">LOG IN</a>
+    </div>
+
     <div class="container">
-
-
+        <img src="img/logo-big-v3.png" alt="">
+        <button class="button" onclick="window.location.href='kanban.html'">naar kanban-bord</button>
     </div>
 
 </body>
-
 </html>
