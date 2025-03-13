adbf3a21cefa0b32c87add910c5f39bf3a552004
Tycho3589 <D289993@edu.curio.nl>
2025-03-13 09:20:18 +0100

    aanmaak van kanban bord
    begin gemaakt aan pagina kanban bord
    en kleine veranderingen aan de rest van de code

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php require_once 'config.exemple.php'; ?>
+    <?php require_once 'backend/config.php'; ?>
     <title>Home</title>
     <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/main.css">
@@ -17,7 +17,7 @@
 
     <div class="container">
         <img src="img/logo-big-v3.png" alt="">
-        <button class="button" onclick="window.location.href='kanban.html'">naar kanban-bord</button>
+        <button class="button" onclick="window.location.href='kanban-bord.php'">naar kanban-bord</button>
     </div>
 
 </body>
---
+++ kanban-bord.php
@@ -0,0 +1,25 @@
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php require_once 'backend/config.php'; ?>
+    <title>Home</title>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+
+</head>
+
+<header>
+
+
+<img src="img/logo-big-v4.png" alt="">
+
+<body>
+
+    <div class="navbar">
+        <a href="index.php">HOME</a>
+        <a href="inlog-page.php">LOG IN</a>
+    </div>
+    <p></p>
+</body>
+</html>
