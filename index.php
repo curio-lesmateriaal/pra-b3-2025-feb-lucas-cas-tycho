f8fed105e6083cccbfef518ea966862a54fd21e5
casy20 <cas.buckens@outlook.com>
2025-03-13 09:19:29 +0100

    img in de navbar gedaan

--- index.php
+++ index.php
@@ -3,16 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php require_once 'config.exemple.php'; ?>
+    <?php require_once 'backend/config.php'; ?>
     <title>Home</title>
     <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
 
-    <div class="navbar">
-        <a href="index.php">HOME</a>
-        <a href="inlog-page.php">LOG IN</a>
+<div class="navbar">
+        <img src="img/logo-big-v4.png" alt="LOGO">
+        <div class="menu">
+            <a href="index.php">HOME</a>
+            <a href="inlog-page.php">LOG IN</a>
+        </div>
     </div>
 
     <div class="container">
