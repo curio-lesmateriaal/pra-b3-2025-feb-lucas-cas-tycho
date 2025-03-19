424e544effc3fe3a7546132105b8929bcea5b185
casy20 <cas.buckens@outlook.com>
2025-03-19 14:16:23 +0100

    taskInfoTodo

--- resources/views/meldingen/kanban-bord.php
+++ resources/views/meldingen/kanban-bord.php
@@ -22,8 +22,8 @@
                 <div class="title">
                     <h2>TO DO</h2>
                 </div>
-                    <div class="tasks-container">
-                      
+                <div class="tasks-container">
+
 
                     <div class="task">Taak 1 <span class="delete-task">✖</span></div>
                     <div class="task">Taak 2 <span class="delete-task">✖</span></div>
@@ -41,12 +41,12 @@
                     </div>
                     <div class="modal-body">
                         <div class="input-group">
-                            <label for="add-task-todo">taak naam</label>
-                            <input type="text" id="add-task-todo" name="add-task-todo" required>
+                            <label for="taskInfoTodo">taak naam</label>
+                            <input type="text" id="taskInfoTodo" name="taskInfoTodo" required>
                         </div>
                         <div class="input-group">
-                            <label for="task-info-todo">taak info</label>
-                            <textarea id="task-info-todo" name="task-info-todo" style="width: 300px; height: 100px; resize: none;" required></textarea>
+                            <label for="taskInfoTodo">taak info</label>
+                            <textarea id="taskInfoTodo" name="taskInfoTodo" style="width: 300px; height: 100px; resize: none;" required></textarea>
                         </div>
                         <div class="buttons task-buttons">
                             <button type="button" class="task-button" data-close-button>Cancel</button>
@@ -61,26 +61,6 @@
                 </div>
                 <div class="tasks-container">
                 </div>
-                <button data-modal-target="#modal-inprogress" class="add-task">+</button>
-                <div class="modal" id="modal-inprogress">
-                    <div class="modal-header">
-                        <h2>taak maken</h2>
-                    </div>
-                    <div class="modal-body">
-                        <div class="input-group">
-                            <label for="add-task-inprogress">taak naam</label>
-                            <input type="text" id="add-task-inprogress" name="add-task-inprogress" required>
-                        </div>
-                        <div class="input-group">
-                            <label for="task-info-inprogress">taak info</label>
-                            <textarea id="task-info-inprogress" name="task-info-inprogress" style="width: 300px; height: 100px; resize: none;" required></textarea>
-                        </div>
-                        <div class="buttons task-buttons">
-                            <button type="button" class="task-button" data-close-button>Cancel</button>
-                            <button type="submit" class="task-button">Add Task</button>
-                        </div>
-                    </div>
-                </div>
             </div>
             <div class="kanban-column">
                 <div class="title">
@@ -88,34 +68,10 @@
                 </div>
                 <div class="tasks-container">
                 </div>
-                <button data-modal-target="#modal-done" class="add-task">+</button>
-                <div class="modal" id="modal-done">
-                    <div class="modal-header">
-                        <h2>taak maken</h2>
-                    </div>
-
-                   
-                <form action="<?php echo $base_url; ?>meldingenController.php" method="POST">
-                    <div class="modal-body">
-                        <div class="input-group">
-                            <label for="add-task-done">taak naam</label>
-                            <input type="text" id="add-task-done" name="add-task-done" required>
-                        </div>
-                        <div class="input-group">
-                            <label for="task-info-done">taak info</label>
-                            <textarea id="task-info-done" name="task-info-done" style="width: 300px; height: 100px; resize: none;" required></textarea>
-                        </div>
-                        <div class="buttons task-buttons">
-                            <button type="button" class="task-button" data-close-button>Cancel</button>
-                            <button type="submit" class="task-button">Add Task</button>
-                        </div>
-                   
-                </form> 
-                    </div>
-                </div>
-                <div id="overlay"></div>
             </div>
+            <div id="overlay"></div>
         </div>
+    </div>
     </div>
 
     <script src="../../../js/add-task.js"></script>
